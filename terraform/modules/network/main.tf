71a98cc2117b2c5a3b3fe5030f0c71e631f70b6e
Sri Lingala <srilingala1@gmail.com>
2026-01-22 10:22:02 +0000

    proj-3-feat:fixing aks platform

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -18,3 +18,32 @@ resource "azurerm_subnet" "aks" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.aks_subnet_cidr]
 }
+
+resource "azurerm_subnet" "bastion" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.bastion_subnet_cidr]
+}
+
+resource "azurerm_public_ip" "bastion" {
+  name                = "${var.name_prefix}-bastion-pip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = var.tags
+}
+
+resource "azurerm_bastion_host" "bastion" {
+  name                = "${var.name_prefix}-bastion"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
+
+  ip_configuration {
+    name                 = "bastion-ip"
+    subnet_id            = azurerm_subnet.bastion.id
+    public_ip_address_id = azurerm_public_ip.bastion.id
+  }
+}
--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,3 +1,7 @@
 output "aks_subnet_id" {
   value = azurerm_subnet.aks.id
 }
+
+output "bastion_host_id" {
+  value = azurerm_bastion_host.bastion.id
+}
