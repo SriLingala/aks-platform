a0b8f4fa2df1689055abfdcc2f1f7dc12df644dc
Sri Lingala <srilingala1@gmail.com>
2026-01-23 13:30:04 +0000

    proj-3-feat:adding bootstrap and destroys

--- terraform/modules/bootstrap/main.tf
+++ terraform/modules/bootstrap/main.tf
@@ -13,15 +13,15 @@ resource "random_string" "suffix" {
 }
 
 resource "azurerm_storage_account" "state" {
-  name                     = local.storage_account_name
-  resource_group_name      = azurerm_resource_group.state.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-  min_tls_version          = "TLS1_2"
+  name                      = local.storage_account_name
+  resource_group_name       = azurerm_resource_group.state.name
+  location                  = var.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  account_kind              = "StorageV2"
+  min_tls_version           = "TLS1_2"
   enable_https_traffic_only = true
-  tags                     = var.tags
+  tags                      = var.tags
 }
 
 resource "azurerm_storage_container" "state" {
