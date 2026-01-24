d0a9e0fb860bbc6c9ff1c7d141c845216a25f1bb
Sri Lingala <srilingala1@gmail.com>
2026-01-24 06:00:10 +0000

    proj-3-feat:Fixed version issues

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -15,7 +15,7 @@ module "jumpbox" {
   # put jumpbox into same subnet for simplicity (ok for portfolio)
   subnet_id = module.network.aks_subnet_id
 
-  vm_size        = "Standard_B2s"
+  vm_size        = var.jumpbox_vm_size
   admin_username = var.jumpbox_admin_username
   ssh_public_key = var.jumpbox_ssh_public_key
   tags           = local.tags
