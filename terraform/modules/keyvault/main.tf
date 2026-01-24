654aa39e5a8e3591853ec109ce4017429c60dd06
Sri Lingala <srilingala1@gmail.com>
2026-01-24 06:11:32 +0000

    proj-3-feat:Fixed version issues

--- terraform/modules/keyvault/main.tf
+++ terraform/modules/keyvault/main.tf
@@ -1,5 +1,30 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
+
+resource "random_string" "kv_suffix" {
+  length  = 6
+  upper   = false
+  lower   = true
+  numeric = true
+  special = false
+}
+
+locals {
+  keyvault_name = substr(
+    replace(lower("${var.name_prefix}kv${random_string.kv_suffix.result}"), "-", ""),
+    0,
+    24
+  )
+}
+
 resource "azurerm_key_vault" "kv" {
-  name                       = replace("${var.name_prefix}-kv", "-", "")
+  name                       = local.keyvault_name
   location                   = var.location
   resource_group_name        = var.resource_group_name
   tenant_id                  = var.tenant_id
